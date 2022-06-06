ee74ae17abadc390b982b8fcb402a862fe660ecf
return 0 <34759856+nara9006@users.noreply.github.com>
2022-06-06 15:23:02 +0900

    [Bronze V] Title: 사칙연산, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.cc
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int A, B;
+    cin >> A;
+    cin >> B;
+    cout << A+B <<"\n"<< A-B <<"\n"<< A*B<<"\n" << A/B <<"\n"<< A%B;
+    return 0;
+}
+ 
